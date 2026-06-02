c618aa568c29bb4e90dd395e4bfbfd9777c6d200
봉하운 <qjdldhsl12333@gmail.com>
2026-06-02 14:52:58 +0900

    Split footer CSS from global stylesheet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.css';
+import './styles/footer.css';
 import './styles/header.css';
 import './styles/hero.css';
 import './styles/studio.css';
